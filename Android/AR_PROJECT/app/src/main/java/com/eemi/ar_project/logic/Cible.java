bb31b9f2614f8f5f65364e595cb0c68eb1b3663d
Jeremy Curtis <weyoz.dev@gmail.com>
2016-01-27 18:14:14 +0100

    [codage de class]->codage des methode de la class cible

--- Android/AR_PROJECT/app/src/main/java/com/eemi/ar_project/logic/Cible.java
+++ Android/AR_PROJECT/app/src/main/java/com/eemi/ar_project/logic/Cible.java
@@ -1,7 +1,34 @@
 package com.eemi.ar_project.logic;
 
+import java.util.Date;
+
 /**
  * Created by moufle on 27/01/16.
  */
 public class Cible {
+    public double position;
+    public double rayon;
+    public double vitesse;
+    public int type;
+    public boolean active;
+    public int damage;
+    public double born;
+    public double dead;
+
+    public void maj() {
+        
+    }
+
+    public double lifespan() {
+        if (active) {
+            return dead - born;
+        } else {
+            return new Date().getTime() - born;
+        }
+
+    }
+
+    public boolean hitTest( double phi, double theta) {
+        return false; // TODO: 1/27/2016
+    }
 }
