64edf2ddf9ba877e0a2a9963314c199422e32c1b
jordanlamantia <jordan.lamantia@eemi.com>
2016-01-27 16:42:43 +0100

    Projet ios

---
+++ ar-ios/ar-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ar-ios
+//
+//  Created by Jordan La mantia on 27/01/16.
+//  Copyright © 2016 Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
