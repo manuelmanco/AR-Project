04cc34cda9f3bfe17f887827b46883232919bf40
charlesduvert <charles.duvert@gmail.com>
2016-02-02 16:06:13 +0100

    version 1 iOS

---
+++ IOS/ar-ios/ARPTir.m
@@ -0,0 +1,21 @@
+//
+//  ARPTir.m
+//  ar-ios
+//
+//  Created by etudiant on 27/01/2016.
+//  Copyright (c) 2016 Jordan. All rights reserved.
+//
+
+#import "ARPTir.h"
+
+@implementation ARPTir
+
+- (double) distance {
+    return 1.5;
+}
+
+- (int) NmbreDePoints {
+    return 1;
+}
+
+@end
---
+++ IOS/ar-ios/ARPcible.h
@@ -0,0 +1,34 @@
+//
+//  ARPcible.h
+//  ar-ios
+//
+//  Created by charles duvert on 27/01/2016.
+//  Copyright © 2016 Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARPcible : UIViewController
+<<<<<<< Updated upstream
+//POSITION
+@property (nonatomic)  NSMutableArray *position;
+//RAYON
+@property (nonatomic) double rayon;
+//VITESSE
+@property (nonatomic) double vitesse;
+//TYPE
+@property (nonatomic) int type;
+//ACTIVE
+@property (nonatomic) BOOL active;
+//DOMAGE
+@property (nonatomic) int domage;
+//BORN
+@property NSDate *born;
+//DEED
+@property NSDate *deed;
+=======
+    
+
+
+>>>>>>> Stashed changes
+@end
