66e8b097da30f1237478b950fc8c6ef874c6f8f3
KaitaniRiku <kevin.vacherot@etudiant-eemi.com>
2016-02-03 16:20:24 +0100

    ajout d'un dossier contenant les fichiers test du webservice

---
+++ Misc/test_webservice/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<title>Test API</title>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
+    <link rel="icon" href="http://icons.iconarchive.com/icons/musett/dragon-ballz/256/Dragon-Ball-icon.png">
+	</head>
+  <body>
+            <h1>Test webservice</h1>
+
+            <hr/>
+
+			<p>Register Webservice</p>
+            <button class="trigger_event" display="reg" called-method='authentificateDevelopper'>Register</button>
+            <div class="results" id="reg"></div>
+
+			<br/><br/><br/>
+
+			<p>Afficher top 10 scores</p>
+            <button class="trigger_event" display="scores" called-method='getHighScores'>Get top 10</button>
+            <div class="results" id="scores"></div>
+
+            <br/><br/><br/>
+
+			<p>Sauvegarder score</p>
+			<form class="formulaire" action="#" method="post">
+				<input class="player_name" type="text" name="player_name" value="" placeholder="Name">
+				<input class="player_score" type="text" name="player_score" value="" placeholder="Score">
+                <button class="trigger_event" display="update_results" called-method='saveScore'>Save Score</button>
+            </form>
+            <div class="results" id="update_results"></div>
+
+            <br/><br/>
+
+	       <script type="text/javascript" src="jquery-1.11.3.min.js"></script>
+	    	<script type="text/javascript" src="main.js"></script>
+	    </body>
+</html>
