daf9a8e6c0b617cfbd87734d810cdbfe618d94c2
Jeremy Curtis <weyoz.dev@gmail.com>
2016-02-03 14:21:21 +0100

    modification position double xyz

--- Android/AR_PROJECT/app/src/main/java/com/eemi/ar_project/logic/Cible.java
+++ Android/AR_PROJECT/app/src/main/java/com/eemi/ar_project/logic/Cible.java
@@ -6,7 +6,9 @@ import java.util.Date;
  * Created by moufle on 27/01/16.
  */
 public class Cible {
-    public double position;
+    public double x;
+    public double y;
+    public double z;
     public double rayon;
     public double vitesse;
     public int type;
