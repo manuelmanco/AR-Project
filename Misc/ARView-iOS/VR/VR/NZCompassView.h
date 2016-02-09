418f37b67cebd07b72bb9cc98a908c5992d53758
zino <zino@poulet.org>
2016-02-09 11:12:20 +0100

    VR for iOS

---
+++ Misc/ARView-iOS/VR/VR/NZCompassView.h
@@ -0,0 +1,14 @@
+//
+//  NZCompassView.h
+//  ADP-VR
+//
+//  Created by Nico Zino on 2012/10/11.
+//  Copyright (c) 2012 Nico Zino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NZCompassView : UIView
+@property(nonatomic) CGFloat heading; // in DEGREES
+
+@end
---
+++ Misc/ARView-iOS/VR/VR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VR
+//
+//  Created by Nicolas Zinovieff on 2016/02/09.
+//  Copyright © 2016 Nicolas Zinovieff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
