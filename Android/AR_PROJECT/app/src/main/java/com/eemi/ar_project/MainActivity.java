14e16bb6c7bd9c5033c60774ca217abfdc77c5e2
Jeremy Curtis <weyoz.dev@gmail.com>
2016-02-10 16:40:03 +0100

    generation des vues, almost done, still coding

--- Android/AR_PROJECT/app/src/main/java/com/eemi/ar_project/MainActivity.java
+++ Android/AR_PROJECT/app/src/main/java/com/eemi/ar_project/MainActivity.java
@@ -1,7 +1,9 @@
 package com.eemi.ar_project;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView score = (TextView) findViewById(R.id.textViewScoreText);
+        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/sui-generis-rg.ttf");
+        score.setTypeface(face);
     }
 }
