9df77bd48ef222d739509625a8172522ef12d0f3
charlesduvert <charles.duvert@gmail.com>
2016-02-10 18:09:01 +0100

    high score

--- IOS/ar-ios/ScoreController.m
+++ IOS/ar-ios/ScoreController.m
@@ -22,6 +22,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSError* error = nil;
+    NSData *jsonData2 = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://vacherot.etudiant-eemi.com/perso/ar_project/?developper_token=6c3a235a40020e288792b158896db4bc&module=score&action=getHighScores&params%5Blimit%5D=0&params%5Boffset%5D=10&params%5Bperiod%5D=allTime"]];
+    
+    id jsonObjects2 = [NSJSONSerialization JSONObjectWithData:jsonData2 options:NSJSONReadingMutableContainers error:&error];
+    if (error) {
+        
+        NSLog(@"error");
+        
+    } else {
+        NSArray *player = [jsonObjects2  objectForKey:@"player"];
+        NSArray *highscore = [jsonObjects2 objectForKey:@"player_highscore"];
+        
+        /*for (NSDictionary *item in highscore)
+         
+        {*/
+        self.textScore.text = @"Hello World!";
+        //NSLog(@"Data has loaded successfully.");
+    }
+    
+
     // Do any additional setup after loading the view.
 }
 
